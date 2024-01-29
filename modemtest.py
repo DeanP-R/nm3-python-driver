68eb6a191827b02702f90e92844333beed6bd856
DeanP-R <Deanrowlett00@gmail.com>
2024-01-29 11:51:37 +0000

    changed modem names

--- modemtest.py
+++ modemtest.py
@@ -3,16 +3,16 @@ import serial
 
 # Initialize the modems
 serial_port_1 = serial.Serial('/dev/ttyUSB0', 9600)
-modem1 = NM3(serial_port=serial_port_1)
+left_modem = NM3(serial_port=serial_port_1)
 
 serial_port_2 = serial.Serial('/dev/ttyUSB1', 9600)
-modem2 = NM3(serial_port=serial_port_2)
+right_modem = NM3(serial_port=serial_port_2)
 
 # Set addresses
 # The exact method to set the address depends on the functions provided by the driver
 # For example, it might be something like:
-modem1.set_address(1)
-modem2.set_address(2)
+left_modem.set_address(1)
+right_modem.set_address(2)
 
 # Your code to interact with the modems
 # ...
