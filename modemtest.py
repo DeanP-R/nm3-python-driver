617b8b3b7eca9e1da5d0334de3be13cd50fc8296
DeanP-R <Deanrowlett00@gmail.com>
2024-01-29 11:52:40 +0000

    tidying

--- modemtest.py
+++ modemtest.py
@@ -7,13 +7,3 @@ left_modem = NM3(serial_port=serial_port_1)
 
 serial_port_2 = serial.Serial('/dev/ttyUSB1', 9600)
 right_modem = NM3(serial_port=serial_port_2)
-
-# Set addresses
-# The exact method to set the address depends on the functions provided by the driver
-# For example, it might be something like:
-left_modem.set_address(1)
-right_modem.set_address(2)
-
-# Close the serial ports
-serial_port_1.close()
-serial_port_2.close()
